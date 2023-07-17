7f61a9dd3c78559006ae72a8dfd6899acb7d19dd
taro-28 <tarotaro0208@gmail.com>
2023-07-17 13:57:31 +0900

    move file

---
+++ src/app/server-action/browser-validation/page.tsx
@@ -0,0 +1,10 @@
+import { Form } from "./Form";
+
+export default function Page() {
+  return (
+    <main>
+      <h1 className="text-2xl font-bold">Browser Validation</h1>
+      <Form />
+    </main>
+  );
+}
