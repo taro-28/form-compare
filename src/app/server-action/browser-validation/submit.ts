f121cce2fc8cc4f85478cd8929b195ea75fc825a
taro-28 <tarotaro0208@gmail.com>
2023-08-05 13:28:19 +0900

    fix

--- src/app/server-action/browser-validation/submit.ts
+++ src/app/server-action/browser-validation/submit.ts
@@ -1,7 +1,6 @@
 "use server";
 
 export const submit = async (data: FormData) => {
-  if (data.get("age") === "30") throw new Error("Invalid input.");
   const name = data.get("name");
   const age = data.get("age");
   const email = data.get("email");
